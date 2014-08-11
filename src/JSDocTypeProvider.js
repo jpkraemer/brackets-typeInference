e4b0c626c4ec34d7097c9d1712a194cf05e8ec72
Jan-Peter Kraemer <jkramer@adobe.com>
2014-08-11 10:10:17 -0700

    JSDoc now also parsed when adding function identifiers

--- src/JSDocTypeProvider.js
+++ src/JSDocTypeProvider.js
@@ -61,23 +61,25 @@
 		for (var i = 0; i < unindexedFunctions.length; i++) {
 			var functionInfo = unindexedFunctions[i]; 
 			var functionName = functionInfo.name || "anonymous";
-			var functionIdentifier = document.file.fullPath + "-function-" + functionName + "-" + Math.random().toString(36).substr(2,5);
-			var functionIdentifierJSDoc = " * " + TypeInformationJSDocRenderer.functionIdentifierToJSDoc(functionIdentifier) + "\n";
+			var typeInformation = {
+				functionIdentifier: document.file.fullPath + "-function-" + functionName + "-" + Math.random().toString(36).substr(2,5),
+				file: document.file.fullPath
+			};
+			var functionIdentifierJSDoc = " * " + TypeInformationJSDocRenderer.functionIdentifierToJSDoc(typeInformation.functionIdentifier) + "\n";
 			var commentInset; 
 			if (functionInfo.commentRange !== undefined) {
-				var commentText = document.getRange(functionInfo.commentRange.start, functionInfo.commentRange.end);
-				var commentAsTypeInformation = TypeInformationJSDocRenderer.updateTypeInformationWithJSDoc({}, commentText);
-				commentInset = document.getRange({ line: functionInfo.commentRange.start.line, ch: 0 }, functionInfo.commentRange.start);
-				document.replaceRange(commentInset + functionIdentifierJSDoc, { line: functionInfo.commentRange.end.line, ch: 0 });
+				var commentAsTypeInformation = _updateTypeInformationWithCommentRange({}, document, functionInfo.commentRange);
+				_.merge(typeInformation, commentAsTypeInformation);
+				if (commentAsTypeInformation.functionIdentifier === undefined) {
+					commentInset = document.getRange({ line: functionInfo.commentRange.start.line, ch: 0 }, functionInfo.commentRange.start);
+					document.replaceRange(commentInset + functionIdentifierJSDoc, { line: functionInfo.commentRange.end.line, ch: 0 });
+				}
 			} else {
 				commentInset = document.getRange({ line: functionInfo.functionRange.start.line, ch: 0 }, functionInfo.functionRange.start);
 				var newCommentText = "\n" + commentInset + "/**\n" + commentInset + functionIdentifierJSDoc + commentInset + " */";
 				document.replaceRange(newCommentText, functionInfo.functionRange.start);
 			}
-			results.push({
-				file: document.file.fullPath,
-				functionIdentifier: functionIdentifier
-			});
+			results.push(typeInformation);
 		}
 
 		if (! documentWasDirty) {
@@ -95,14 +97,18 @@
 	function _parseInformationFromDocument (document, indexedFunctions) {
 		var results = [];
 
-		_.forOwn(indexedFunctions, function (functionLocation, functionIdentifier) {
-			var commentText = document.getRange(functionLocation.commentRange.start, functionLocation.commentRange.end);
-			commentText = commentText.replace(/^\s*(?:\*\/?|\/\*\*)/mg, "");
-			var typeInformation = TypeInformationJSDocRenderer.updateTypeInformationWithJSDoc({ file: document.file.fullPath }, commentText);
+		_.forOwn(indexedFunctions, function (functionInfo, functionIdentifier) {
+			var typeInformation = _updateTypeInformationWithCommentRange({ file: document.file.fullPath }, document, functionInfo.commentRange);
 			results.push(typeInformation);
 		});
 		
 		$(exports).trigger("didReceiveTypeInformation", [ results ]); 
+	}
+
+	function _updateTypeInformationWithCommentRange (typeInformation, document, commentRange) {
+		var commentText = document.getRange(commentRange.start, commentRange.end);
+		commentText = commentText.replace(/^\s*(?:\*\/?|\/\*\*)/mg, "");
+		return TypeInformationJSDocRenderer.updateTypeInformationWithJSDoc(typeInformation, commentText);	
 	}
 
 	exports.init = init; 
