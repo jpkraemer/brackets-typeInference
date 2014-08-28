87e6db5e87c5979201e3af1fe85f52ac058d8f3f
Jan-Peter Kraemer <jkramer@adobe.com>
2014-08-28 15:02:59 -0700

    added nice animations

--- src/TestCaseWidget.js
+++ src/TestCaseWidget.js
@@ -4,6 +4,7 @@
 define(function (require, exports, module) {
 	"use strict"; 
 
+	var _ 					= require("./lib/lodash");
 	var Resizer 			= brackets.getModule("utils/Resizer");
 	var CodeMirror 			= brackets.getModule("thirdparty/CodeMirror2/lib/codemirror");
 
@@ -11,8 +12,11 @@ define(function (require, exports, module) {
 	 * @constructor
 	 * Renders a single test case. 
 	 * @param {{title: string, isSuggestion: boolean}} options
+	 * @param {Function} initializationDoneCallback called after the code mirror instance was properly initialized
 	 */
-	function TestCaseWidget (options) {
+	function TestCaseWidget (options, initializationDoneCallback) {
+		_.bindAll(this);
+
 		var testCaseTemplate = require("text!./templates/testCase.html");
 
 		this.$container = $(Mustache.render(testCaseTemplate, options));
@@ -27,14 +31,38 @@ define(function (require, exports, module) {
             lineWrapping: true,
 			value: require("text!./TestCasesPane.js")
 		});
-
-		setTimeout(this.codeMirror.refresh.bind(this.codeMirror), 1);
 	}
 
 	TestCaseWidget.prototype.constructor = TestCaseWidget; 
 
 	TestCaseWidget.prototype.codeMirror = undefined;
 	TestCaseWidget.prototype.$container = undefined;
+
+	TestCaseWidget.prototype.insertBefore = function(element) {
+		this.$container.height(0); 
+		this.$container.insertBefore(element);
+
+		this.codeMirror.on("update", function () {
+			if (this.$container.find('.ti-header').outerHeight() >= 46) {
+				//this is a pretty dirty hack to check if layout has happened. .ti-header has a
+				//min-height of 46, so css has been applied if it's at least this big. 
+
+				var height = 	this.$container.find('.ti-header').outerHeight() + 
+								this.$container.find('.ti-editorHolder').outerHeight() +
+								4; //padding-bottom
+
+				this.$container.animate({ height: height }, 'fast', function () {
+					this.$container.removeAttr('style');
+				}.bind(this));
+
+				this.codeMirror.off("update");
+			}
+		}.bind(this));
+
+		setTimeout(function () {
+			this.codeMirror.refresh();
+		}.bind(this), 1);
+	};
 
 	module.exports = TestCaseWidget;  
 });
--- src/TestCasesPane.js
+++ src/TestCasesPane.js
@@ -4,24 +4,24 @@
 define(function (require, exports, module) {
 	"use strict"; 
 
+	var _ 					= require("./lib/lodash");
 	var Resizer 			= brackets.getModule("utils/Resizer");
 	var CodeMirror 			= brackets.getModule("thirdparty/CodeMirror2/lib/codemirror");
 	var TestCaseWidget		= require("./TestCaseWidget");
 
 	function TestCasesPane () {
+		_.bindAll(this);
+
 		this.$pane = $(require("text!./templates/TestCasesPane.html"));
 		this.$scollView = this.$pane.find('.ti-scrollView');
 		Resizer.makeResizable(this.$pane.get(0), Resizer.DIRECTION_HORIZONTAL, "left");
 
+		this.$pane.find('.ti-roundAddButton').on('click', this.addTestButtonClicked); 
+
+		this.addTestButtonClicked();
+
 		this.$pane.insertBefore('#editor-holder > .CodeMirror');
 		$('#editor-holder > .CodeMirror').width("50%");
-
-		var testTestCaseWidget = new TestCaseWidget({
-			title: "This is a test case", 
-			isSuggestion: false
-		});
-
-		this.$scollView.prepend(testTestCaseWidget.$container); 
 	}
 
 	TestCasesPane.prototype.constructor = TestCasesPane; 
@@ -31,6 +31,21 @@ define(function (require, exports, module) {
 	TestCasesPane.prototype.codeMirror = undefined;
 
 	TestCasesPane.prototype.testCaseTemplate = undefined; 
+
+
+	TestCasesPane.prototype.addTestButtonClicked = function(event) {
+		var newWidget = new TestCaseWidget({
+			title: "This is a test case", 
+			isSuggestion: false
+		});
+
+		newWidget.insertBefore(this.$pane.find('.ti-roundAddButton'));
+		
+		if (event !== undefined) {
+			event.stopPropagation();
+		}
+	};
+
 
 	module.exports = TestCasesPane;  
 });
